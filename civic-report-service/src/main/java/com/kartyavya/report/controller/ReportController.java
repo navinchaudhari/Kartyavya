0843edcc772cb27fb578e09f7fa68cd535f41c3c
ShivrajHalgode <halgodeshivraj03@gmail.com>
2026-07-27 03:38:27 +0530

    Completed Day 1 setup for civic report service

---
+++ civic-report-service/src/main/java/com/kartyavya/report/controller/ReportController.java
@@ -0,0 +1,46 @@
+package com.kartyavya.report.controller;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.kartyavya.report.entity.Report;
+import com.kartyavya.report.service.ReportService;
+
+
+@RestController
+@RequestMapping("/reports")
+public class ReportController {
+
+
+private final ReportService service;
+
+
+public ReportController(ReportService service){
+
+    this.service=service;
+
+}
+
+
+
+@GetMapping
+public List<Report> getReports(){
+
+    return service.getAllReports();
+
+}
+
+
+
+@PostMapping
+public Report createReport(
+        @RequestBody Report report){
+
+    return service.saveReport(report);
+
+}
+
+
+}
