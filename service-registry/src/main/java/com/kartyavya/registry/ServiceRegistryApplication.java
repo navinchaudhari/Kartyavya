7b937b5bef062c733b06d231c4fc205368527a52
Tanushka <tanushkapatil2003@gmail.com>
2026-08-08 03:00:00 +0530

    Updated service-registry module

--- service-registry/src/main/java/com/kartyavya/registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/kartyavya/registry/ServiceRegistryApplication.java
@@ -1,14 +1,15 @@
 package com.kartyavya.registry;
 
+import com.kartyavya.contracts.config.EnvFileLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-// Owner: M5 - do not add business logic here.
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
     public static void main(String[] args) {
+        EnvFileLoader.load();
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
 }
