1e1b7730eb3431f7b2f54d5ccbb563256ca527a1
navinchaudhari <navin.chaudhari.cmfeb26@gmail.com>
2026-08-07 16:59:44 +0530

    mod: config-server

--- config-server/src/main/java/com/kartyavya/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/kartyavya/config/ConfigServerApplication.java
@@ -1,14 +1,15 @@
 package com.kartyavya.config;
 
+import com.kartyavya.contracts.config.EnvFileLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-// Owner: M1 (Project Lead) - do not add business logic here.
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
+        EnvFileLoader.load();
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
